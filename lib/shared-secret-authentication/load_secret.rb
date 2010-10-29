40a7cac3239ac0556519452f6974f37d49381440
Josh Moore <joshsmoore@gmail.com>
2010-10-29 11:58:01 +0800

    cleaned up the loading of SHARED_SECRET

--- lib/shared-secret-authentication/load_secret.rb
+++ lib/shared-secret-authentication/load_secret.rb
@@ -1,5 +1,1 @@
-require 'yaml'
-
-puts File.new('config/shared_secret.yml').read
-puts YAML.load(File.new('config/shared_secret.yml')).inspect
-SHARED_SECRET = YAML.load(File.new('config/shared_secret.yml'))
+SHARED_SECRET = YAML.load(File.new('config/shared_secret.yml'))['shared_secret']
