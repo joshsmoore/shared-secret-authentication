6eb411501ffa9b8c63b6ab030095756473fd855f
Mason Chang <changmason@gmail.com>
2013-05-20 17:54:27 +0800

    enhance compatibility with ruby 1.9

--- lib/shared-secret-authentication/hash_signatures.rb
+++ lib/shared-secret-authentication/hash_signatures.rb
@@ -7,7 +7,7 @@ module SharedSecretAuthentication
 
   def self.hash_signature_correct?(hash)
     raise ArgumentError.new("hash must be signed (have a key of 'signature')") unless expected_signature = hash.delete('signature')
-    
+
     hash_signature(hash) == expected_signature
   end
 
@@ -15,14 +15,13 @@ module SharedSecretAuthentication
     d = Digest::SHA2.new
     string_keys = hash.keys.inject({}) {|keys, key| keys.merge!(key.to_s => key) }
     string_keys.keys.sort.each do |key|
-      puts key.inspect
       d.update key
       value = hash[string_keys[key]]
+
       if value.instance_of? Hash
-        value = value.collect {|k,v| 
+        value = value.collect {|k,v|
           if v.respond_to? :strftime
             if v.respond_to? :utc
-
               k.to_s + v.utc.strftime('%a %b %m %H:%M:%S %Y')
             else
               k.to_s + v.strftime('%a %b %m %H:%M:%S %Y')
@@ -30,31 +29,29 @@ module SharedSecretAuthentication
           else
             k.to_s + v.to_s
           end
-        }
+        }.sort.join
       end
-      
+
       if value.instance_of? Array
-        puts value.inspect
-        value = value.sort
         value = value.collect {|v|
           if v.respond_to? :strftime
             if v.respond_to? :utc
-              puts 'here in utc'
               v.utc.strftime('%a %b %m %H:%M:%S %Y')
             else
-              puts 'not in utc'
               v.strftime('%a %b %m %H:%M:%S %Y')
             end
           else
             v.to_s
           end
-        
-        }
+        }.sort.join
       elsif value.respond_to? :strftime
-        value = value.strftime('%a %b %m %H:%M:%S %Y')
+        if value.respond_to? :utc
+          value = value.utc.strftime('%a %b %m %H:%M:%S %Y')
+        else
+          value = value.strftime('%a %b %m %H:%M:%S %Y')
+        end
       end
-      puts 'in here'
-      puts value.to_s.inspect
+
       d.update value.to_s
     end
     d.update SHARED_SECRET
